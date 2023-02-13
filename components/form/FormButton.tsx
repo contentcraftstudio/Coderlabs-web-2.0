c3b46e7637d1c4b94428e53d53632b236c23e92e
Josemi <josemi.perezsolano@gmail.com>
2023-02-12 22:19:33 -0500

    add::Form button

---
+++ components/form/FormButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useFormikContext } from 'formik'
+
+type Props = {}
+
+export default function FormButton ({}: Props) {
+  const { submitForm } = useFormikContext()
+  return (
+    <button
+    className=' w-[133px] h-[44px] rounded-[8px] bg-background-dark text-white text-[14px] font-primary font-medium text-center '
+      type='submit'
+      onClick={() => submitForm()}
+    >Submit
+    </button>
+  )
+}
