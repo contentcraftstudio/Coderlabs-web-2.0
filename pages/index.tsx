5d0e97e37d49c9c69a18273d5fb27b392ce16bbb
Josemi <jose@coderlabs.co>
2023-02-15 11:09:12 -0500

    shore:: ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ import ProfileEdit from '@/components/SwiperTeam'
 import Team from '@/components/Team'
 // import { useTranslation } from 'react-i18next'
 
-
 export default function Home () {
   // const { t } = useTranslation()
 
