1868222d03063ae57fb55d5a65778f0783342b99
Samuel Rodriguez <99931293+samuelrdg11@users.noreply.github.com>
2023-03-15 15:29:57 -0500

    add:: add button file in components

---
+++ components/form/Button.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+type Props = {
+  label: string
+  variant: string
+}
+
+export default function IButton({ label, variant }: Props) {
+  return (
+    variant === 'contained' ? (
+    <button
+      className='w-[103px] h-[36px] bg-orange rounded-[4px] text-white text-[14px] font-primary text-center'
+      variant={variant}
+    >
+      {label}
+    </button>
+
+    ) : variant === 'outlined' ? (
+      <button
+        className='w-[190px] h-[37px] outline outline-1 outline-orange rounded-[4px] text-orange text-[14px] font-primary text-center flex items-center px-4 py-2 gap-1 '
+        variant={variant}
+      >
+        {label}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20" height="20">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+        </svg>
+      </button>
+    ) : null
+  )
+}
