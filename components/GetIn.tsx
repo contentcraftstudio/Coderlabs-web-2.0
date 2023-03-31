424c72f7f8edffd9a452847e84e55ed6b069f06b
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-03-31 18:01:36 -0500

    Change::GetIn.tsx

--- components/GetIn.tsx
+++ components/GetIn.tsx
@@ -1,25 +1,43 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import Form from './form/Form'
-import FormInput from './form/FormInput'
+import React from "react"
+import { useTranslation } from "react-i18next"
+import Form from "./form/Form"
+import FormInput from "./form/FormInput"
 
 type Props = {}
 
-export default function GetIn ({}: Props) {
+export default function GetIn({}: Props) {
   const { t } = useTranslation()
   return (
     <Form
       initialValues={{
-        name: '',
-        phone: '',
-        email: '',
-        contacting: '',
-        budget: '',
-        menssage: ''
+        name: "",
+        phone: "",
+        email: "",
+        contacting: "",
+        budget: "",
+        menssage: "",
       }}
     >
-      <div className=' w-full bg-white rounded px-4 py-[20px] sm:space-y-[20px]' >
-        <FormInput label={t('name')} name='name' type='name' />
+      <div className=' w-full bg-white rounded px-4 py-[20px] sm:space-y-[20px]'>
+        <div className='mb-62'>
+          <p className='w-225 font-roboto text-4xl font-medium leading-10 text-gray-700'>
+            Get In Touch
+          </p>
+        </div>
+        <div className='columns-2'>
+          <FormInput label={t("Name")} name='name' type='name' />
+          <FormInput label={t("Phone_number")} name='phone' type='phone' />
+        </div>
+        <div className='columns-2'>
+          <FormInput label={t("Email")} name='email' type='email' />
+          <FormInput
+            label={t("Contacting_us")}
+            name='contacting'
+            type='contacting'
+          />
+        </div>
+        <FormInput label={t("Defined_budget")} name='budget' type='budget' />
+        <FormInput label={t("Your_budget")} name='menssage' type='menssage' />
       </div>
     </Form>
   )
