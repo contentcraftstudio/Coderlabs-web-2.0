c7e643c51144ce6fb38fed94f38f422e0d9451ed
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-03 17:36:12 -0500

    Change::FormInput.tsx

--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -11,7 +11,7 @@ export default function FormInput({ name, label, type }: Props) {
   const { setFieldTouched, setFieldValue, values } = useFormikContext()
 
   return (
-    <div className='w-full rounded-[8px]'>
+    <div className='w-full rounded-[8px] flex h-[63px]'>
       <input
         type={type}
         id={name}
@@ -20,6 +20,7 @@ export default function FormInput({ name, label, type }: Props) {
         value={getIn(values, name)}
         onChange={(e) => setFieldValue(name, e.target.value)}
         onBlur={() => setFieldTouched(name)}
+        className='px-[12px] w-full rounded-[8px] bg-gray-50 py-23 border border-gray-200/50'
       />
     </div>
   )
