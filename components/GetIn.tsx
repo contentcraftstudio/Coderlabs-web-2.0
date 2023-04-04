1ee99f863a57f57f58b126457e91ea3dc85915ed
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-04 16:08:07 -0500

    Change::GetIn.tsx

--- components/GetIn.tsx
+++ components/GetIn.tsx
@@ -1,16 +1,66 @@
-import React from "react"
+import React, { useState } from "react"
 import { useTranslation } from "react-i18next"
 import Form from "./form/Form"
 import FormInput from "./form/FormInput"
-import Button from "./Button"
 import FormButton from "./form/FormButton"
+import axios from 'axios'
 
 type Props = {}
 
 export default function GetIn({}: Props) {
   const { t } = useTranslation()
+
+  const [formularioEnviado, setFormularioEnviado] = useState(false)
+
+  const validate = (values) => {
+    const errors = {}
+    if (!values.name) {
+      errors.name = "Required"
+    }
+    if (!values.phone) {
+      errors.phone = "Required"
+    } else if (!/^[0-9]*$/.test(values.phone)) {
+      errors.phone = "Invalid phone number"
+    }
+    if (!values.email) {
+      errors.email = "Required"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = "Invalid email address"
+    }
+    if (!values.contacting) {
+      errors.contacting = "Required"
+    }
+    if (!values.budget) {
+      errors.budget = "Required"
+    }
+    if (!values.message) {
+      errors.message = "Required"
+    }
+    return errors
+  }
+
+  const handleValue = async (values, action) => {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "https://sheet.best/api/sheets/d7b94f3f-7644-43e3-a223-038b9d06d2da",
+        data: {
+          ...values,
+        },
+      })
+      console.log(response.status)
+      if (response.status === 200) {
+        setFormularioEnviado(true)
+        action.resetForm()
+        setTimeout(() => setFormularioEnviado(false), 4000)
+      }
+    } catch (error) {
+      console.log("err", error)
+    }
+  }
+
   return (
-    <div className="w-[100%] px-6 container">
+    <div className='w-[100%] px-6 container'>
       <Form
         initialValues={{
           name: "",
@@ -20,6 +70,7 @@ export default function GetIn({}: Props) {
           budget: "",
           menssage: "",
         }}
+        onSubmit={handleValue}
       >
         <div className='w-full bg-white rounded px-[20px] py-[20px] sm:space-y-[20px]'>
           <p className='w-225 font-roboto text-4xl font-medium leading-10 text-gray-700 mb-[42px]'>
