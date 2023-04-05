65ce4bfb03d586839aca118aa307c6100208464d
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-05 15:48:11 -0500

    Change::Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable no-undef */
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import Image from 'next/image'
-import logo from '../public/assets/llavelogo.svg'
-import logobajo from '../public/assets/rectangle.svg'
-import facebook from '../public/assets/facebook.svg'
-import instagram from '../public/assets/instagram.svg'
-import twitter from '../public/assets/twitter.svg'
-import linkedin from '../public/assets/linkedin.svg'
+import React from "react"
+import { useTranslation } from "react-i18next"
+import Image from "next/image"
+import logo from "../public/assets/llavelogo.svg"
+import logobajo from "../public/assets/rectangle.svg"
+import facebook from "../public/assets/facebook.svg"
+import instagram from "../public/assets/instagram.svg"
+import twitter from "../public/assets/twitter.svg"
+import linkedin from "../public/assets/linkedin.svg"
 
 type Props = {}
 
-export default function Footer ({}: Props): JSX.Element {
+export default function Footer({}: Props): JSX.Element {
   const { t } = useTranslation()
 
   return (
@@ -22,30 +22,46 @@ export default function Footer ({}: Props): JSX.Element {
           alt='coderlabs'
           className=' self-start mt-[6px] lg:mt-[10px]  sm:w-[20px] lg:w-[20px] sm:h-[46px]  md:h-[50px] lg:h-[70px]'
         />
-        <h3 className='font-secondary ml-[-8px]  sm:text-[40px] lg:text-[48px] xl:text-[64px] font-bold text-white px-1'>{t('Navigate')}</h3>
+        <h3 className='font-secondary ml-[-8px]  sm:text-[40px] lg:text-[48px] xl:text-[64px] font-bold text-white px-1'>
+          {t("Navigate")}
+        </h3>
       </div>
-      <div className='py-[22px] flex justify-start gap-[40px] flex-wrap '>
+      <div className='py-[22px] flex justify-start gap-[40px] flex-wrap'>
         <a href='#'>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>{t('Identity')}</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>
+            {t("Identity")}
+          </p>
         </a>
         <a href='#'>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>{t('Services')}</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>
+            {t("Services")}
+          </p>
         </a>
         <a href='#'>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>{t('Tools')}</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>
+            {t("Tools")}
+          </p>
         </a>
         <a href='#'>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>{t('Advantages')}</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>
+            {t("Advantages")}
+          </p>
         </a>
         <a href='#'>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>{t('Team')}</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>
+            {t("Team")}
+          </p>
         </a>
         <a href='#'>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>{t('Blog')}</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300 sm:text-start xl:text-center'>
+            {t("Blog")}
+          </p>
         </a>
       </div>
       <div className='py-[18px] flex justify-start '>
-        <h3 className='font-secondary sm:text-[40px] lg:text-[48px] xl:text-[64px] font-bold text-white px-1'>{t('Contact')}</h3>
+        <h3 className='font-secondary sm:text-[40px] lg:text-[48px] xl:text-[64px] font-bold text-white px-1'>
+          {t("Contact")}
+        </h3>
         <Image
           src={logobajo}
           alt='coderlabs'
@@ -54,15 +70,23 @@ export default function Footer ({}: Props): JSX.Element {
       </div>
       <div className='py-[18px] flex flex-col md:flex-row gap-4 justify-between flex-wrap px-[8px] lg:items-center'>
         <div className='flex flex-col md:flex-row justify-between gap-[16px] '>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300'>contact@coderlabs.co</p>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-100'>1234567890</p>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-100'>Barranquilla</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-300'>
+            contact@coderlabs.co
+          </p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-100'>
+            1234567890
+          </p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-100'>
+            Barranquilla
+          </p>
         </div>
         <div className='md:text-center sm:py-4 md:py-0'>
-          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-100 '>Book a call</p>
+          <p className='font-primary sm:text-[16px] lg:text-[18px] xl:text-[20px] font-medium text-gray-100 '>
+            Book a call
+          </p>
         </div>
         <div className='sm:text-center flex justify-between gap-[30px]'>
-          <a href='#'>
+          <a href='https://www.instagram.com/coderlabs_/' target='_blank'>
             <Image
               src={instagram}
               alt='coderlabs instagram'
@@ -71,7 +95,7 @@ export default function Footer ({}: Props): JSX.Element {
               style={{ marginBlockEnd: 24 }}
             />
           </a>
-          <a href='#'>
+          <a href='https://www.facebook.com/coderlabs.code' target='_blank'>
             <Image
               src={facebook}
               alt='coderlabs facebook'
@@ -80,7 +104,7 @@ export default function Footer ({}: Props): JSX.Element {
               style={{ marginBlockEnd: 24 }}
             />
           </a>
-          <a href='#'>
+          <a href='https://twitter.com/coderlabs_' target='_blank'>
             <Image
               src={twitter}
               alt='coderlabs twitter'
@@ -89,7 +113,7 @@ export default function Footer ({}: Props): JSX.Element {
               style={{ marginBlockEnd: 24 }}
             />
           </a>
-          <a href='#'>
+          <a href='https://www.linkedin.com/company/coderlabs/' target='_blank'>
             <Image
               src={linkedin}
               alt='coderlabs linkedin'
@@ -101,7 +125,9 @@ export default function Footer ({}: Props): JSX.Element {
         </div>
       </div>
       <div className='py-[18px]'>
-        <p className='font-primary sm:text-[12px] lg:text-[14px] xl:text-[16px] font-medium text-gray-400 text-center'>2023 CoderLabs SAS. Developing with love Barranquilla, Colombia.</p>
+        <p className='font-primary sm:text-[12px] lg:text-[14px] xl:text-[16px] font-medium text-gray-400 text-center'>
+          2023 CoderLabs SAS. Developing with love Barranquilla, Colombia.
+        </p>
       </div>
     </div>
   )
