39e3d6bb6b1f141424551eeb34c7e26e3f0d747f
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-17 17:52:23 -0500

    Change::ToolsSec.tsx

--- components/ToolsSec.tsx
+++ components/ToolsSec.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import TechnologiesGrid from './TechnologiesGrid'
+import React from "react"
+import { useTranslation } from "react-i18next"
+import TechnologiesGrid from "./TechnologiesGrid"
 
 type Props = {}
 
@@ -8,10 +8,14 @@ export default function ToolsSec({}: Props) {
   const { t } = useTranslation()
 
   return (
-    <section className='grid sm:grid-cols-1 xl:grid-cols-2 gap-2 py-[32px] sm:mx-[30px] md:mx-[62px] lg:mx-[115px]'>
+    <section className='grid sm:grid-cols-1 xl:grid-cols-2 gap-2 py-[32px] px-6'>
       <div className=' sm:pb-[50px] md:pb-[30px] sm:text-center xl:text-start xl:divide-y-2 divide-gray-800 px-4'>
-        <h3 className=' font-secondary font-medium sm:text-[34px] lg:text-[48px] sm:pb-4 xl:pb-[40px] '>{t('Tools_and_technologies')}</h3>
-        <p className=' font-primary font-normal sm:text-[16px] lg:text-[20px] lg:pt-[60px] '>{t('paragraph_tools')}</p>
+        <h3 className=' font-secondary font-medium sm:text-[34px] lg:text-[48px] sm:pb-4 xl:pb-[40px] '>
+          {t("Tools_and_technologies")}
+        </h3>
+        <p className=' font-primary font-normal sm:text-[16px] lg:text-[20px] lg:pt-[60px] '>
+          {t("paragraph_tools")}
+        </p>
       </div>
       <div className='h-max'>
         <TechnologiesGrid />
