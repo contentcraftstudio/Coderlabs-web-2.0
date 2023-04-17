0e5c95b331f4892375d229786c47964dba1ad704
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-17 18:30:47 -0500

    Change::ToolsSec.tsx

--- components/ToolsSec.tsx
+++ components/ToolsSec.tsx
@@ -10,7 +10,7 @@ export default function ToolsSec({}: Props) {
   return (
     <section className='grid sm:grid-cols-1 xl:grid-cols-2 gap-2 py-[32px] px-[103px]'>
       <div className=' sm:pb-[50px] md:pb-[30px] sm:text-center xl:text-start xl:divide-y-2 divide-gray-800 px-4'>
-        <h3 className=' font-secondary font-medium sm:text-[34px] lg:text-[48px] sm:pb-4 xl:pb-[40px] '>
+        <h3 className='font-secondary font-medium sm:text-[34px] lg:text-[48px] sm:pb-4 xl:pb-[40px]'>
           {t("Tools_and_technologies")}
         </h3>
         <p className=' font-primary font-normal sm:text-[16px] lg:text-[20px] lg:pt-[60px] '>
