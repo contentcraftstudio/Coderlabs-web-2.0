cfd4bff22e041cd6b6cc7404c456e2a9d311d545
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-17 18:11:02 -0500

    Change::Cards.tsx

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -10,7 +10,7 @@ export default function Cards ({}: Props) {
   return (
     <>
       <div className='sm:pb-[0px] lg:pb-[10px]'>
-        <h2 className='font-primary font-mediun text-center sm:text-[34px] md:text-[48px] xl:text-[60px] mb-[40px]'>{t('How_we_do')}</h2>
+        <h2 className='text-center font-secondary font-medium sm:text-[34px] lg:text-[48px] mb-[40px]'>{t('How_we_do')}</h2>
         <p className=' font-secondary font-normal text-center sm:text-[16px] md:text-[20px] sm:w-[330px] md:w-[630px] xl:w-[730px] mx-auto'>{t('paragraph_focus')}</p>
       </div>
       <div className='flex justify-center flex-wrap gap-[30px]'>
