320b40610493413293bcea38dc24c638301ba584
Samuel Rodriguez <99931293+samuelrdg11@users.noreply.github.com>
2023-04-18 15:40:21 -0500

    change:: font and weight

--- components/Team.tsx
+++ components/Team.tsx
@@ -9,8 +9,8 @@ export default function Team ({}: Props) {
   return (
     <>
       <div className='flex sm:flex-col md:flex-row justify-between mt-[40px] pt-[40px] border-t-2 border-white ml-6'>
-        <h3 className='font-primary font-normal sm:text-[16px] md:text-[20px]  text-orange sm:pb-[32px]'>{t('Our_team')}</h3>
-        <p className='sm:w-[280px] md:w-[330px] xl:w-[430px] font-primary text-white font-normal sm:text-[16px] md:text-[20px] lg:mr-40'>{t('paragraph_our_team')}</p>
+        <h3 className='font-secondary font-medium sm:text-[16px] md:text-[20px]  text-orange sm:pb-[32px]'>{t('Our_team')}</h3>
+        <p className='sm:w-[280px] md:w-[330px] xl:w-[406px] font-primary text-white font-normal sm:text-[16px] md:text-[20px] lg:mr-40'>{t('paragraph_our_team')}</p>
       </div>
       <ProfileEdit />
     </>
