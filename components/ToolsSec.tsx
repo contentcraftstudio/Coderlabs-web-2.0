9573c08ce2a8f7b3cf44280260767823dff2f378
Samuel Rodriguez <99931293+samuelrdg11@users.noreply.github.com>
2023-04-20 18:02:21 -0500

    change:: width

--- components/ToolsSec.tsx
+++ components/ToolsSec.tsx
@@ -13,7 +13,7 @@ export default function ToolsSec({}: Props) {
       className='grid sm:grid-cols-1 xl:grid-cols-2 gap-[80px] py-[32px] sm:mx-[30px] md:mx-[62px] '
     >
       <div className=' sm:pb-[50px] md:pb-[30px] sm:text-center xl:text-start xl:divide-y-2 divide-gray-800 px-4'>
-        <h3 className='font-secondary font-medium sm:text-[34px] md:text-[48px] sm:pb-4 xl:pb-[40px] sm:mb-[20px]'>
+        <h3 className='font-secondary font-medium sm:text-[34px] xl:w-[702px] md:text-[48px] sm:pb-4 xl:pb-[40px] sm:mb-[20px]'>
           {t("Tools_and_technologies")}
         </h3>
         <p className=' font-primary font-normal sm:text-[16px] md:text-[20px] lg:pt-[60px] sm:leading-[19px] md:leading-[23px]'>
