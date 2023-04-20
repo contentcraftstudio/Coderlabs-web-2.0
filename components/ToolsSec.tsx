063678d269f7cbf65c96aab0bceca033df6beb1e
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-20 15:52:14 -0500

    Change::ToolSec.tsx

--- components/ToolsSec.tsx
+++ components/ToolsSec.tsx
@@ -8,12 +8,15 @@ export default function ToolsSec({}: Props) {
   const { t } = useTranslation()
 
   return (
-    <section id='tools' className='grid sm:grid-cols-1 xl:grid-cols-2 gap-2 py-[32px] sm:mx-[30px] md:mx-[62px] '>
+    <section
+      id='tools'
+      className='grid sm:grid-cols-1 xl:grid-cols-2 gap-2 py-[32px] sm:mx-[30px] md:mx-[62px] '
+    >
       <div className=' sm:pb-[50px] md:pb-[30px] sm:text-center xl:text-start xl:divide-y-2 divide-gray-800 px-4'>
         <h3 className='font-secondary font-medium sm:text-[34px] lg:text-[48px] sm:pb-4 xl:pb-[40px] sm:mb-[20px]'>
           {t("Tools_and_technologies")}
         </h3>
-        <p className=' font-primary font-normal sm:text-[16px] lg:text-[20px] lg:pt-[60px] sm:leading-[19px] md:leading-[19px] xl:leading-[23px]'>
+        <p className=' font-primary font-normal sm:text-[16px] md:text-[20px] lg:pt-[60px] sm:leading-[19px] md:leading-[23px]'>
           {t("paragraph_tools")}
         </p>
       </div>
