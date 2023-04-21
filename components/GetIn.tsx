87461ab8a4104ec2a392413f108b948646619af8
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-21 18:11:17 -0500

    Change::Get In Touch

--- components/GetIn.tsx
+++ components/GetIn.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react"
 import { useTranslation } from "react-i18next"
+import { ErrorMessage } from "formik"
 import Form from "./form/Form"
 import FormInput from "./form/FormInput"
 import FormButton from "./form/FormButton"
 import axios from "axios"
+import * as Yup from "yup"
 
 type Props = {}
 
@@ -12,32 +14,16 @@ export default function GetIn({}: Props) {
 
   const [formularioEnviado, setFormularioEnviado] = useState(false)
 
-  const validate = (values) => {
-    const errors = {}
-    if (!values.name) {
-      errors.name = "Required"
-    }
-    if (!values.phone) {
-      errors.phone = "Required"
-    } else if (!/^[0-9]*$/.test(values.phone)) {
-      errors.phone = "Invalid phone number"
-    }
-    if (!values.email) {
-      errors.email = "Required"
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = "Invalid email address"
-    }
-    if (!values.contacting) {
-      errors.contacting = "Required"
-    }
-    if (!values.budget) {
-      errors.budget = "Required"
-    }
-    if (!values.message) {
-      errors.message = "Required"
-    }
-    return errors
-  }
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Required"),
+    phone: Yup.string()
+      .required("Required")
+      .matches(/^[0-9]*$/, "Invalid phone number"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    contacting: Yup.string().required("Required"),
+    budget: Yup.string().required("Required"),
+    message: Yup.string().required("Required"),
+  })
 
   const handleValue = async (values, action) => {
     try {
@@ -71,37 +57,58 @@ export default function GetIn({}: Props) {
           menssage: "",
         }}
         onSubmit={handleValue}
+        validationSchema={validationSchema}
       >
-        <div className='w-full bg-white rounded px-[20px] py-[20px] sm:space-y-[20px]'>
-          <p className='w-225 font-secondary text-4xl font-[530] leading-10 text-gray-700 mb-[42px]'>
-            {t("Get_in_touch")}
-          </p>
-          <div className='flex gap-[20px] flex-wrap'>
-            <div className='lg:w-[48.5%] sm:w-full xl:w-[49.2%]'>
-              <FormInput label={t("Name")} name='name' type='name' />
+        {({ errors, touched }) => (
+          <div className='w-full bg-white rounded px-[20px] py-[20px] sm:space-y-[20px]'>
+            <p className='w-225 font-secondary text-4xl font-[530] leading-10 text-gray-700 mb-[42px]'>
+              {t("Get_in_touch")}
+            </p>
+            <div className='flex gap-[20px] flex-wrap'>
+              <div className='lg:w-[48.5%] sm:w-full xl:w-[49.2%]'>
+                <FormInput label={t("Name")} name='name' type='name' />
+                <ErrorMessage name='name' />
+              </div>
+              <div className='lg:w-[48.5%] sm:w-full xl:w-[49.2%]'>
+                <FormInput label={t("Email")} name='email' type='email' />
+                <ErrorMessage name='email' />
+              </div>
             </div>
-            <div className='lg:w-[48.5%] sm:w-full xl:w-[49.2%]'>
-              <FormInput label={t("Email")} name='email' type='email' />
+            <div className='flex gap-[20px] flex-wrap'>
+              <div className='lg:w-[48.5%] sm:w-full md:w-[48.5%] xl:w-[49.2%]'>
+                <FormInput
+                  label={t("Phone_number")}
+                  name='phone'
+                  type='phone'
+                />
+                <ErrorMessage name='phone' />
+              </div>
+              <div className='lg:w-[48.5%] sm:w-full md:w-[48.5%] xl:w-[49.2%]'>
+                <FormInput
+                  label={t("Contacting_us")}
+                  name='contacting'
+                  type='contacting'
+                />
+                <ErrorMessage name='contacting' />
+              </div>
+            </div>
+            <FormInput
+              label={t("Defined_budget")}
+              name='budget'
+              type='budget'
+            />
+            <ErrorMessage name='budget' />
+            <FormInput
+              label={t("Your_budget")}
+              name='menssage'
+              type='menssage'
+            />
+            <ErrorMessage name='message' />
+            <div className='pt-[42px] flex justify-end pb-[10px]'>
+              <FormButton />
             </div>
           </div>
-          <div className='flex gap-[20px] flex-wrap'>
-            <div className='lg:w-[48.5%] sm:w-full md:w-[48.5%] xl:w-[49.2%]'>
-              <FormInput label={t("Phone_number")} name='phone' type='phone' />
-            </div>
-            <div className='lg:w-[48.5%] sm:w-full md:w-[48.5%] xl:w-[49.2%]'>
-              <FormInput
-                label={t("Contacting_us")}
-                name='contacting'
-                type='contacting'
-              />
-            </div>
-          </div>
-          <FormInput label={t("Defined_budget")} name='budget' type='budget' />
-          <FormInput label={t("Your_budget")} name='menssage' type='menssage' />
-          <div className='pt-[42px] flex justify-end pb-[10px]'>
-            <FormButton />
-          </div>
-        </div>
+        )}
       </Form>
     </div>
   )
