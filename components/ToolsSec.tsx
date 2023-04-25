de95b26c669378e38b6af8153b196a5d6be72a1e
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-04-25 16:30:56 -0500

    Change::Fixed technology section

--- components/ToolsSec.tsx
+++ components/ToolsSec.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import TechnologiesGrid from './TechnologiesGrid'
 
 export default function ToolsSec () {
   const { t } = useTranslation()
 
+  const [widthTitle, setWidthTitle] = useState('lg:w-[702px]')
+
+  useEffect(() => {
+    function handleResize () {
+      if (window.innerWidth >= 976 && window.innerWidth <= 1650) {
+        setWidthTitle('lg:w-[500px]')
+      } else {
+        setWidthTitle('lg:w-[702px]')
+      }
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <section
       id='tools'
-      className='grid sm:grid-cols-1 xl:grid-cols-2 gap-[80px] py-[32px] md:mx-[62px]'
+      className='grid sm:grid-cols-1 xl:grid-cols-2 gap-[80px] py-[32px] md:px-[62px] lg:px-[103px]'
     >
       <div className='sm:text-center xl:text-start xl:divide-y-2 divide-gray-800 px-4'>
-        <h3 className='font-secondary font-medium sm:text-[34px] xl:w-[702px] md:text-[48px] xl:pb-[40px] sm:mb-[20px]'>
+        <h3 className={`font-secondary font-medium sm:text-[34px] md:text-[48px] xl:pb-[40px] sm:mb-[20px] ${widthTitle}`}>
           {t('Tools_and_technologies')}
         </h3>
         <p className='font-primary font-normal sm:text-[16px] md:text-[20px] lg:pt-[60px] sm:leading-[19px] md:leading-[23px]'>
