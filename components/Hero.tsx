268eb3d5fbbd135cf4a79d484e861ba4b1d97e3c
barrosjss <jesus.reinaldo.barros@gmail.com>
2023-05-26 13:18:45 -0500

    Change::Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero () {
               {t('paragraph_hero')}
             </p>
             <div className='flex'>
-              <a href='https://www.coderlabs.co/ai' target='_blank' rel='noreferrer'>
+              <a href='https://www.linkedin.com/company/coderlabsco/' rel='noreferrer'>
                 <IButton label={t('Make_it_happen')} variant='contained' />
               </a>
               <div className='mr-[20px]' />
