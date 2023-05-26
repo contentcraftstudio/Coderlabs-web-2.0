f0f876ed7bf05d8cd3256336817dc6c5225ea2bd
barrosjss <jesus.reinaldo.barros@gmail.com>
2023-05-26 13:18:56 -0500

    Change::Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
-import React from 'react'
-import coderlabsLogo from '../public/assets/coderlabs_logo.svg'
-import SwitchButton from './SwitchButton'
+import Image from "next/image"
+import React from "react"
+import coderlabsLogo from "../public/assets/coderlabs_logo.svg"
+import SwitchButton from "./SwitchButton"
+import IButton from "./form/Button"
 
-export default function Navbar () {
+export default function Navbar() {
   return (
     <nav
       id='nav'
@@ -14,7 +15,10 @@ export default function Navbar () {
         alt='coderlabs logo'
         className='sm:w-[35px] sm:h-[32px] md:w-[45px] md:h-[40px] xl:w-[52px] xl:h-[48px]'
       />
-      <div>
+      <div className='flex'>
+        <a href='/ai' rel='noreferrer'>
+          <IButton label='Try our AI chat!' variant='text' />
+        </a>
         <SwitchButton />
       </div>
     </nav>
