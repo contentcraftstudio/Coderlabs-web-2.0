925974fd0a1ace6172673a9a5c021aba0a6282ed
Samuel <samuelrdg11@gmail.com>
2023-05-26 09:32:27 -0500

    change:: funtionality chat ai

--- components/ChatAI.tsx
+++ components/ChatAI.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Chat from './Chat'
 import InfoChat from './InfoChat'
 
 export default function ChatAI() {
-  const [chat, setChat] = React.useState(false);
-  // const { screens } = require('tailwindcss/defaultTheme');
-  // const mobile = screens.sm
-
-
-  return (
-    <InfoChat />
-  )
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 })
+  
+  useEffect(() => {
+    function handleResize() {
+      setScreenSize({ width: window.innerWidth, height: window.innerHeight })
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+  console.log(screenSize)
+  if (screenSize.width <= 1000) {
+    return <Chat />
+  } else {
+    return <InfoChat />
+  }
 }
