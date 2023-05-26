1e2f48f112211194163b4f151b98e1f04787d228
Samuel <samuelrdg11@gmail.com>
2023-05-26 09:33:32 -0500

    change:: chat styles

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -17,28 +17,28 @@ export default function Chat() {
 
   return (
     <div>
-      <div className="p-4">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`message ${index % 2 === 0 ? 'sender' : 'receiver'} p-2 mb-2`}
-          >
-            {message}        
-          </div>
-        ))}
+      <div className="flex flex-col h-screen p-4">
+        <div className="flex-1 overflow-y-auto px-4 py-2">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${index % 2 === 0 ? 'sender' : 'receiver'} p-2 mb-2`}
+            >
+              {message}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="fixed bottom-[125px] ml-[35px] bg">
+      <div className="fixed bottom-[95px] lg:px-[70px] md:px-[50px] sm:px-[32px]">
         <div className="relative">
           <input
             type="text"
             placeholder="Escribe"
             value={newMessage}
             onChange={handleInputChange}
-            className="py-2 px-4 border bg-gray-input border-[#DBDBDB] rounded-[8px] pr-12 focus:outline-none w-[1200px]"
+            className="py-2 px-4 border bg-gray-input border-[#DBDBDB] rounded-[8px] pr-12 focus:outline-none md:w-[700px] sm:w-[330px] xl:w-[1120px]"
           />
           <button
             onClick={handleSendMessage}
-            className="absolute right-0 mt-2 mr-2 py-[2px] px-4 bg-blue-500 rounded-[8px] bg-[#5EA496] text-white text-primary "
+            className="absolute right-0 mt-2 mr-2 py-[2px] px-4 bg-blue-500 rounded-[8px] bg-[#5EA496] text-white text-primary"
           >
             Send
           </button>
