694e9fe11d5c2868a0279b823f985395faffdc72
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2023-06-01 14:45:04 -0500

    bugfix::re-added google analytics to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import 'swiper/css'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src='https://www.googletagmanager.com/gtag/js?id=G-460KCDGW86'
+      />
+      <Script id='ga' strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || []
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date())
+        
+          gtag('config', 'G-460KCDGW86')
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
