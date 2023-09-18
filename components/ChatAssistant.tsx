24f36a4fd8a1c82fc01e86df0fa992431d07e91e
Josemi <josemi.perezsolano@gmail.com>
2023-09-18 17:14:09 -0500

    Update ChatAssistant.tsx

--- components/ChatAssistant.tsx
+++ components/ChatAssistant.tsx
@@ -1,26 +1,37 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Skeleton } from 'antd'
-import ChatBox from './ChatBox'
+import { Button, Form, Input, Skeleton, Typography } from 'antd'
 import { motion } from 'framer-motion'
+import { useRequest } from 'ahooks'
+import HTMLRenderer from 'react-html-renderer'
+import axios from 'axios'
 
 function ChatAssistant () {
-  const [responses, setResponses] = useState([])
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [form] = Form.useForm()
+  const [responses, setResponses] = useState()
+  // const [data, setData] = useState([])
   const [screenSize, setScreenSize] = useState({ width: 0, height: 0 })
 
-  console.log('🚀 ~ file: Chat.tsx:10 ~ Chat ~ data:', data)
-  const listRef = useRef(null)
-  useEffect(() => {
-    fetch('https://cdn.contentful.com/spaces/8rxe1sxxuabo/environments/master/entries?content_type=coderlabs', {
-      headers: {
-        Authorization: 'Bearer wIvUEV88zWPT7HNKdiSEfLFWlOgq8i5Uvpp8LdxNv5s'
+  const { loading, run } = useRequest(
+    async params =>
+      await axios.post('https://cors-anywhere.herokuapp.com/https://node-ai-9d3892bd650a.herokuapp.com/ai', params, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        }
+      }),
+    {
+      manual: true,
+      onError (e) {
+        const mgs = e.response?.data
+        console.log(mgs)
+      },
+      onSuccess: ({ data }) => {
+        console.log('🚀 ~ file: ChatAssistant.tsx:29 ~ ChatAssistant ~ data:', data)
+        setResponses(data?.choices[0]?.message?.content)
       }
-    })
-      .then((response) => response.json())
-      .then((result) => setData(result.items))
-      .catch((error) => console.error(error))
-  }, [])
+    }
+  )
 
   useEffect(() => {
     function handleResize () {
@@ -33,19 +44,14 @@ function ChatAssistant () {
     }
   }, [])
 
-  const handleChatResponse = (item: any) => {
-    setResponses((): any[] => [...item])
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+    run({
+      about: values.about,
+      link: values.link,
+      cta: values.cta
+    })
   }
-
-  const loadingQuery = (item: boolean): void => {
-    setLoading(item)
-  }
-
-  useEffect(() => {
-    if (listRef.current) {
-      listRef.current.scrollTop = listRef.current.scrollHeight
-    }
-  }, [responses])
 
   return (
     <>
@@ -56,14 +62,33 @@ function ChatAssistant () {
         transition={{ ease: 'easeOut', duration: 3 }}
         style={{ display: 'flex', height: screenSize.width <= 700 ? '90vh' : '98vh', flexDirection: 'column', justifyContent: 'space-between', paddingBottom: screenSize.width <= 700 ? 40 : 10 }}
       >
-        <div className='chat-messages-container' style={{ flex: 1, overflowY: 'auto', padding: '6px 12px' }} ref={listRef}>
+        <div className='chat-messages-container' style={{ flex: 1, overflowY: 'auto', padding: '6px 12px' }}>
           <div>
             {/* Web View */}
-            {loading ? <Skeleton active style={{ marginBlockEnd: 100, width: '90%' }} /> : null}
+            {loading ? <Skeleton active style={{ marginBlockEnd: 100, width: '90%' }} /> : <HTMLRenderer html={responses} />}
           </div>
         </div>
-        <ChatBox selection={data[0]?.fields} onResponse={handleChatResponse} onLoading={loadingQuery} />
-
+        <div style={{ width: '100%' }}>
+          <Form
+            layout='inline'
+            form={form}
+            onFinish={onFinish}
+            style={{ maxWidth: 'none', flexDirection: 'row', justifyContent: 'space-between' }}
+          >
+            <Form.Item name='about'>
+              <Input placeholder='about' style={{ width: (screenSize.width / 2) / 3.8 }} />
+            </Form.Item>
+            <Form.Item name='link'>
+              <Input placeholder='link ' style={{ width: (screenSize.width / 2) / 3.8 }} />
+            </Form.Item>
+            <Form.Item name='cta'>
+              <Input placeholder='input placeholder' style={{ width: (screenSize.width / 2) / 4 }} />
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType='submit' color='#F3A44D'>Submit</Button>
+            </Form.Item>
+          </Form>
+        </div>
       </motion.div>
     </>
   )
