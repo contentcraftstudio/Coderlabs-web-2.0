f90f00c4c4637a014aedb72060c608761f98ce1b
Armando Guzman <armandoc.guzman@gmail.com>
2023-10-18 10:58:02 -0500

    eslint

--- components/ContactUsDrawer.tsx
+++ components/ContactUsDrawer.tsx
@@ -1,74 +1,73 @@
-import React, { useEffect, useState } from "react";
-import Image from "next/dist/client/image";
-import messageOrange from "../public/assets/tree/message.svg";
-import chevronRight from "../public/assets/tree/chevron-right.svg";
-import closeIcon from "../public/assets/tree/x-mark.svg";
-import shareIcon from "../public/assets/tree/arrow-up-tray.svg";
-import SaveToContacts from "./SaveToContacts";
-import atIcon from "../public/assets/tree/atIcon.svg";
-import phoneIcon from "../public/assets/tree/phoneIcon.svg";
-import locationIcon from "../public/assets/tree/locationIcon.svg";
-import clockIcon from "../public/assets/tree/clockIcon.svg";
+import React, { useEffect, useState } from 'react'
+import Image from 'next/dist/client/image'
+import messageOrange from '../public/assets/tree/message.svg'
+import chevronRight from '../public/assets/tree/chevron-right.svg'
+import closeIcon from '../public/assets/tree/x-mark.svg'
+import shareIcon from '../public/assets/tree/arrow-up-tray.svg'
+import SaveToContacts from './SaveToContacts'
+import atIcon from '../public/assets/tree/atIcon.svg'
+import phoneIcon from '../public/assets/tree/phoneIcon.svg'
+import locationIcon from '../public/assets/tree/locationIcon.svg'
+import clockIcon from '../public/assets/tree/clockIcon.svg'
 
-export default function ContactUsDrawer() {
-  const [isOpen, setIsOpen] = useState(false);
+export default function ContactUsDrawer () {
+  const [isOpen, setIsOpen] = useState(false)
 
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflowY = "hidden"; // Prevent scrolling
+      document.body.style.overflowY = 'hidden' // Prevent scrolling
     } else {
-      document.body.style.overflowY = "auto"; // Enable scrolling
+      document.body.style.overflowY = 'auto' // Enable scrolling
     }
 
     return () => {
-      document.body.style.overflowY = "auto"; // Ensure scrolling is enabled when component unmounts
-    };
-  }, [isOpen]);
+      document.body.style.overflowY = 'auto' // Ensure scrolling is enabled when component unmounts
+    }
+  }, [isOpen])
 
   return (
     <>
       <button
         onClick={() => {
-          setIsOpen(true);
+          setIsOpen(true)
         }}
-        className="flex w-full justify-between items-center px-2 py-1 border-2 rounded-[50px] border-gray-500 text-white font-semibold text-xs"
+        className='flex w-full justify-between items-center px-2 py-1 border-2 rounded-[50px] border-gray-500 text-white font-semibold text-xs'
       >
-        <Image src={messageOrange} alt="Contact Us CoderLabs" />
-        <span className="italic">Contact Us</span>
-        <Image src={chevronRight} alt="Arrow Icon" />
+        <Image src={messageOrange} alt='Contact Us CoderLabs' />
+        <span className='italic'>Contact Us</span>
+        <Image src={chevronRight} alt='Arrow Icon' />
       </button>
       <main
         className={
-          " fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
+          ' fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ' +
           (isOpen
-            ? " transition-opacity opacity-100 duration-500 translate-x-0  "
-            : " transition-all delay-500 opacity-0 translate-y-full  ")
+            ? ' transition-opacity opacity-100 duration-500 translate-x-0  '
+            : ' transition-all delay-500 opacity-0 translate-y-full  ')
         }
       >
         <section
           className={
-            "rounded-t-[20px] w-screen max-w-lg bottom-0 absolute bg-white h-full delay-400 duration-500 ease-in-out transition-all transform  " +
-            (isOpen ? " translate-y-[420px] " : " translate-y-full ")
+            'rounded-t-[20px] w-screen max-w-lg bottom-0 absolute bg-white h-full delay-400 duration-500 ease-in-out transition-all transform  ' +
+            (isOpen ? ' translate-y-[420px] ' : ' translate-y-full ')
           }
         >
-          <article className="relative w-screen max-w-lg pb-10 flex flex-col gap-4 h-full space-y-6 p-6">
+          <article className='relative w-screen max-w-lg pb-10 flex flex-col gap-4 h-full space-y-6 p-6'>
 
-            <section className="w-full flex justify-between items-center">
+            <section className='w-full flex justify-between items-center'>
               <button>
-                <Image src={shareIcon} alt="Share" />
+                <Image src={shareIcon} alt='Share' />
               </button>
-              <p className="uppercase font-semibold text-base">Contact us</p>
+              <p className='uppercase font-semibold text-base'>Contact us</p>
               <button onClick={() => setIsOpen(false)}>
-                <Image src={closeIcon} alt="Close button" />
+                <Image src={closeIcon} alt='Close button' />
               </button>
             </section>
 
-
-            <ul className="flex flex-col gap-4 text-xs font-medium">
-              <li className="flex gap-3 px-2"><Image src={atIcon} alt="Mail" />contact@coderlabs.co</li>
-              <li className="flex gap-3 px-2"><Image src={phoneIcon} alt="Mail" />300 277 1564</li>
-              <li className="flex items-start gap-3 px-2"><Image src={locationIcon} alt="Mail" />Calle 35A No. 17-74, piso 3, Barranquilla, Atlantico, Colombia, 080006.</li>
-              <li className="flex items-start gap-3 px-2"><Image src={clockIcon} alt="Mail" />Lunes - viernes 9:00 a 18:00 Confirmar antes de llegar.</li>
+            <ul className='flex flex-col gap-4 text-xs font-medium'>
+              <li className='flex gap-3 px-2'><Image src={atIcon} alt='Mail' />contact@coderlabs.co</li>
+              <li className='flex gap-3 px-2'><Image src={phoneIcon} alt='Mail' />300 277 1564</li>
+              <li className='flex items-start gap-3 px-2'><Image src={locationIcon} alt='Mail' />Calle 35A No. 17-74, piso 3, Barranquilla, Atlantico, Colombia, 080006.</li>
+              <li className='flex items-start gap-3 px-2'><Image src={clockIcon} alt='Mail' />Lunes - viernes 9:00 a 18:00 Confirmar antes de llegar.</li>
             </ul>
 
             <SaveToContacts />
@@ -76,12 +75,12 @@ export default function ContactUsDrawer() {
 
         </section>
         <section
-          className=" w-screen h-full cursor-pointer "
+          className=' w-screen h-full cursor-pointer '
           onClick={() => {
-            setIsOpen(false);
+            setIsOpen(false)
           }}
-        ></section>
+        />
       </main>
     </>
-  );
+  )
 }
