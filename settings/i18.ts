7bb04ddb367ced4cfd2f34592809e7071e78d880
Julian Garcia <juliangabo1204@gmail.com>
2024-12-23 19:41:23 -0500

    Fix build compile

--- settings/i18.ts
+++ settings/i18.ts
@@ -1,20 +1,19 @@
-
-import i18next from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import { ES } from '@/constants/translate/es'
-import { EN } from '@/constants/translate/en'
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import { ES } from "@/constants/translate/es";
+import { EN } from "@/constants/translate/en";
 
 const resources = {
   es: {
-    translation: ES
+    translation: ES,
   },
   en: {
-    translation: EN
-  }
-}
-
+    translation: EN,
+  },
+};
+//
 i18next.use(initReactI18next).init({
-  lng: 'en', // if you're using a language detector, do not define the lng option
+  lng: "en", // if you're using a language detector, do not define the lng option
   debug: true,
-  resources
-})
+  resources,
+});
