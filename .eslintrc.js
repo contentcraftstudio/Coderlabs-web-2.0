15f34a1b8a75ae3b9a4b0a2e51fdc118124b10ee
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2025-05-08 18:14:45 -0500

    formatting corrections

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    'jsx-quotes': 'off',
+    'quotes': 'off',
+    'semi': 'off',
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'no-trailing-spaces': 'off',
+    'eol-last': 'off'
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
+  eslint: {
+    // Desactivar ESLint durante el build (solo para producción)
+    ignoreDuringBuilds: true,
   },
-};
+  images: {
+    domains: ['coderlabs.co'],
+    unoptimized: process.env.NODE_ENV !== 'production'
+  },
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
